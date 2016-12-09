d0b1cd67c9a0194264b8677004b74a4d8d9178b1
Hew <github@mhs.fastmail.com>
2016-12-09 14:00:31 -0500

    CircleCI Adapter: flesh out pot retrieval functionality

--- includes/ci-adapters/circle-ci-adapter.php
+++ includes/ci-adapters/circle-ci-adapter.php
@@ -1,4 +1,7 @@
 <?php
+
+define( 'LOCALCI_CIRCLECI_API_URL', 'https://circleci.com/api/v1.1' );
+
 class GP_LocalCI_CircleCI_Adapter implements GP_LocalCI_CI_Adapter {
 	private $payload;
 
@@ -20,23 +23,37 @@ class GP_LocalCI_CircleCI_Adapter implements GP_LocalCI_CI_Adapter {
 		);
 	}
 
-	public function get_new_strings_po() {
-		$response = $this->safe_get( $this->payload->build_url );
+	public function get_new_strings_pot() {
+		if ( ! $this->payload->has_artifacts ) {
+			return false;
+		}
+
+		$path  = "{$this->payload->vcs_type}/{$this->payload->username}/{$this->payload->reponame}/{$this->payload->build_num}";
+		$token = GP_LocalCI_Config::get_value( $this->payload->username, $this->payload->reponame, 'build_ci_api_token' );
+		$url   = LOCALCI_CIRCLECI_API_URL . "/project/{$path}/artifacts?circle-token={$token}";
+
+		$response = wp_remote_get( esc_url_raw( $url ) );
 
 		if ( empty( $response ) || is_wp_error( $response ) ) {
-			$this->die_with_error( "Artifact pull failed.", 400 );
-		}
-	}
-
-	public function get_gp_project_id() {
-	}
-
-	public function safe_get( $url ) {
-		if ( ! gp_startswith( $url, 'https://circleci.com' ) ) {
-			return new WP_Error;
+			return false;
 		}
 
-		return wp_remote_get( $url, array() );
+		$artifacts = json_decode( wp_remote_retrieve_body( $response ) );
+		$new_strings_artifact = false;
+
+		foreach ( $artifacts as $artifact ) {
+			if ( '$CIRCLE_ARTIFACTS/translate/localci-new-strings.pot' == $artifact->pretty_path ) {
+				$new_strings_artifact = $artifact;
+				break;
+			}
+		}
+
+		if ( empty( $new_strings_artifact ) ) {
+			return false;
+		}
+
+		$response = wp_remote_get( esc_url_raw( $new_strings_artifact->url ) );
+
+		return wp_remote_retrieve_body( $response );
 	}
 }
-
