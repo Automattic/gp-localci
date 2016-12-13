bebc46efbe4ea97b065f7cc1bfd328c2e5204b81
Hew <github@mhs.fastmail.com>
2016-12-12 22:04:07 -0500

    Streamline CI adapter interface

--- includes/ci-adapters/interface-ci-adapter.php
+++ includes/ci-adapters/interface-ci-adapter.php
@@ -1,11 +1,9 @@
 <?php
 interface GP_LocalCI_CI_Adapter {
+	public function get_payload();
+
 	public function get_gh_data();
 
-	public function get_new_strings_po();
-
-	public function get_gp_project_id();
-
-	public function safe_get( $url );
+	public function get_new_strings_pot();
 }
 
