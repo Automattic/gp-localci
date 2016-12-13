2aaf13a6c09b50111f55efa3155cad88c6db7735
Hew <github@mhs.fastmail.com>
2016-12-13 12:50:00 -0500

    Add coverage summary generation logic

--- includes/localci-functions.php
+++ includes/localci-functions.php
@@ -19,17 +19,44 @@ function localci_load_po( $po_obj_or_file ) {
 
 function localci_generate_coverage_stats( $po_obj_or_file, $coverage ) {
 	$po = localci_load_po( $po_obj_or_file );
-
 	$stats = array_fill_keys( LOCALCI_DESIRED_LOCALES, 0 );
-	$stats['string_count'] = count( $po->entries );
 
 	foreach ( $coverage as $row ) {
 		$stats[$row->locale]++;
 	}
 
+	$num_translated = array_sum( $stats );
+
+	$stats['num_strings'] = count( $po->entries );
+	$stats['percent_translated'] = localci_generate_coverage_percent_translated( $stats['num_strings'], $num_translated );
+	$stats['summary'] = localci_generate_coverage_summary( $stats['num_strings'], $stats['percent_translated'] );
+
 	return $stats;
 }
 
+function localci_generate_coverage_percent_translated( $num_originals, $num_translated ) {
+	return number_format( ( $num_translated / ( count( LOCALCI_DESIRED_LOCALES ) * $num_originals ) ) * 100 );
+}
+
+function localci_generate_coverage_summary( $num_strings, $percent_translated ) {
+	$summary = sprintf( _n( '%s new string. ', '%s new strings. ', $num_strings, 'gp_localci' ), $num_strings );
+
+	switch ( true ) {
+		case $percent_translated == 100:
+			$summary .= "Translations: 100% coverage.";
+			break;
+		case $percent_translated >= 75:
+			$summary .= "Translations: {$percent_translated}% coverage.";
+			break;
+		case $percent_translated > 25:
+			$summary .= "Translations: {$percent_translated}% coverage.";
+			break;
+		case $percent_translated <= 25:
+			$summary .= "Warning -- {$percent_translated}% translated.";
+	}
+
+	return $summary;
+}
 
 function localci_generate_string_suggestions( ) {
 	// @TODO: likely not trivial
