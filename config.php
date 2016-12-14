a15fc565ae39cbd88c665baf9fb509cd07af2868
Hew <hewsut@users.noreply.github.com>
2016-12-13 21:43:25 -0500

    Remove outdated define from config.php

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 
 define( 'LOCALCI_BUILD_CI', 'circleci' );
-define( 'LOCALCI_BUILD_CI_API_TOKEN', '' );
 define( 'LOCALCI_DESIRED_LOCALES', '' );
 define( 'LOCALCI_GITHUB_API_URL', 'https://api.github.com' );
 define( 'LOCALCI_GITHUB_API_MANAGEMENT_TOKEN', '' );
@@ -14,4 +13,3 @@ class GP_LocalCI_Config {
 		return isset( self::$repo_metadata[$fqrn][$key] ) ? self::$repo_metadata[$fqrn][$key] : false;
 	}
 }
-
